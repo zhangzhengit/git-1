c83234116b3b3700b6b38626f3aa8931ddcd4302
zhangzhen <1071324756@qq.com>
2018-11-08 23:32:51 +0800

    20181108

---
+++ src/com/vo/test/Test_1.java
@@ -0,0 +1,16 @@
+package com.vo.test;
+
+import java.time.LocalTime;
+
+/**
+ *
+ * @author zhangzhen
+ * @date 2018年11月8日
+ */
+public class Test_1 {
+
+	public static void main(String[] args) {
+
+		System.out.println(LocalTime.now());
+	}
+}
